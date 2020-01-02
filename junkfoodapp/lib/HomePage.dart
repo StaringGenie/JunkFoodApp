b51a5cfe37af4f2ee620ed60eac5b14d248d337e
Данил Ченцов <danil090598@gmail.com>
2020-01-02 17:33:38 +0300

    move to AppBar

--- junkfoodapp/lib/HomePage.dart
+++ junkfoodapp/lib/HomePage.dart
@@ -12,53 +12,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Padding(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Padding(
           padding: const EdgeInsets.all(14.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: 40,
-                    height: 12,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Container(
-                          height: 3,
-                          width: 25,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(3)),
-                        ),
-                        Container(
-                          height: 3,
-                          width: 15,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(3)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text('THE SPICYCAB',
-                      style: GoogleFonts.montserrat(fontSize: 18)),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/da/da8018aedb0a01f3c17c53a50805275a684c13a1_full.jpg'),
-                  )
-                ],
-              ),
-            ],
+          child: Container(
+            width: 40,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 3,
+                  width: 25,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(3)),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Container(
+                  height: 3,
+                  width: 15,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(3)),
+                ),
+              ],
+            ),
           ),
         ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text('THE SPICYCAB',
+                style: GoogleFonts.montserrat(
+                  textStyle: TextStyle(color: Colors.black),
+                  fontSize: 18,
+                )),
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/da/da8018aedb0a01f3c17c53a50805275a684c13a1_full.jpg'),
+            )
+          ],
+        ),
+        actions: <Widget>[],
       ),
+      body: Container(),
     );
   }
 }
