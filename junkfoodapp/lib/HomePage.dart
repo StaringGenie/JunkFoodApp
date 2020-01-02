35d2abc6805930169229f1623c75b6232e2fefbb
Данил Ченцов <danil090598@gmail.com>
2020-01-02 17:11:49 +0300

    navbar

--- junkfoodapp/lib/HomePage.dart
+++ junkfoodapp/lib/HomePage.dart
@@ -1,4 +1,7 @@
+//design credit https://dribbble.com/shots/9221669/attachments/1265209?mode=media
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +11,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 40,
+                    height: 12,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          height: 3,
+                          width: 25,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(3)),
+                        ),
+                        Container(
+                          height: 3,
+                          width: 15,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(3)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text('THE SPICYCAB',
+                      style: GoogleFonts.montserrat(fontSize: 18)),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/da/da8018aedb0a01f3c17c53a50805275a684c13a1_full.jpg'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
